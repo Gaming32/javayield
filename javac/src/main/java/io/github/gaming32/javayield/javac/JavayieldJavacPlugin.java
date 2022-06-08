c22a11c093609aeb2f346cff2cee68d2c6444e4d
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-06-08 16:40:34 -0500

    Remove instrumentation for Javac

--- javac/src/main/java/io/github/gaming32/javayield/javac/JavayieldJavacPlugin.java
+++ javac/src/main/java/io/github/gaming32/javayield/javac/JavayieldJavacPlugin.java
@@ -1,27 +1,23 @@
 package io.github.gaming32.javayield.javac;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
-import java.lang.instrument.Instrumentation;
-import java.security.ProtectionDomain;
-import java.util.ListIterator;
-import java.util.jar.JarFile;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Field;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ModuleElement;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileManager.Location;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardLocation;
 
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
@@ -30,9 +26,23 @@ import com.sun.source.util.TaskListener;
 import com.sun.source.util.Trees;
 
 import io.github.gaming32.javayield.transform.YieldTransformer;
-import net.bytebuddy.agent.ByteBuddyAgent;
+import sun.misc.Unsafe;
 
 public class JavayieldJavacPlugin implements Plugin {
+    private static final Unsafe UNSAFE;
+    private static final StandardLocation MODULE_SOURCE_PATH = getEnumMemberOrNull(StandardLocation.class, "MODULE_SOURCE_PATH");
+    private static final ElementKind MODULE = getEnumMemberOrNull(ElementKind.class, "MODULE");
+
+    static {
+        try {
+            final Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            UNSAFE = (Unsafe)unsafeField.get(null);
+        } catch (ReflectiveOperationException e) {
+            throw new Error(e);
+        }
+    }
+
     @Override
     public String getName() {
         return "javayield";
@@ -40,55 +50,6 @@ public class JavayieldJavacPlugin implements Plugin {
 
     @Override
     public void init(JavacTask task, String... args) {
-        final Instrumentation inst = ByteBuddyAgent.install();
-        inst.addTransformer(new ClassFileTransformer() {
-            @Override
-            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-                    ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-                if (!className.equals("com/sun/tools/javac/main/JavaCompiler")) {
-                    return null;
-                }
-                try {
-                    final ClassNode clazz = new ClassNode();
-                    new ClassReader(classfileBuffer).accept(clazz, 0);
-                    for (final MethodNode method : clazz.methods) {
-                        if (
-                            method.name.equals("generate") &&
-                            method.desc.equals("(Ljava/util/Queue;Ljava/util/Queue;)V")
-                        ) {
-                            final ListIterator<AbstractInsnNode> it = method.instructions.iterator();
-                            while (it.hasNext()) {
-                                final AbstractInsnNode insn = it.next();
-                                if (insn instanceof MethodInsnNode) {
-                                    final MethodInsnNode methodInsn = (MethodInsnNode)insn;
-                                    if (
-                                        methodInsn.owner.equals("com/sun/tools/javac/main/JavaCompiler") &&
-                                        methodInsn.name.equals("genCode") &&
-                                        methodInsn.desc.equals("(Lcom/sun/tools/javac/comp/Env;Lcom/sun/tools/javac/tree/JCTree$JCClassDecl;)Ljavax/tools/JavaFileObject;")
-                                    ) {
-                                        it.add(new InsnNode(Opcodes.DUP));
-                                        it.add(new FieldInsnNode(
-                                            Opcodes.PUTSTATIC,
-                                            "io/github/gaming32/javayield/javac/LastClassFileHolder",
-                                            "lastClassFile",
-                                            "Ljavax/tools/JavaFileObject;"
-                                        ));
-                                        break;
-                                    }
-                                }
-                            }
-                            break;
-                        }
-                    }
-                    final ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-                    clazz.accept(writer);
-                    return writer.toByteArray();
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                    throw t;
-                }
-            }
-        }, true);
 
         final Trees trees = Trees.instance(task);
         task.addTaskListener(new TaskListener() {
@@ -99,8 +60,55 @@ public class JavayieldJavacPlugin implements Plugin {
             @Override
             public void finished(TaskEvent e) {
                 if (e.getKind() != TaskEvent.Kind.GENERATE) return;
+                final JavaFileManager fileManager;
+                final boolean multiModuleMode;
+                try {
+                    final JavaFileObject fileObject = e.getSourceFile();
+                    final long fieldOffset = UNSAFE.objectFieldOffset(fileObject.getClass().getSuperclass().getDeclaredField("fileManager"));
+                    fileManager = (JavaFileManager)UNSAFE.getObject(fileObject, fieldOffset);
+                    multiModuleMode = MODULE_SOURCE_PATH != null && fileManager.hasLocation(MODULE_SOURCE_PATH);
+                } catch (Exception e1) {
+                    throw new RuntimeException(e1);
+                }
+                final TypeElement classElement = e.getTypeElement();
+                final Location outLocn;
+                final Name nameObject;
+                if (classElement.getEnclosingElement().getKind() == MODULE) {
+                    nameObject = classElement.getQualifiedName();
+                } else {
+                    try {
+                        final long fieldOffset = UNSAFE.objectFieldOffset(classElement.getClass().getDeclaredField("flatname"));
+                        nameObject = (Name)UNSAFE.getObject(classElement, fieldOffset);
+                    } catch (Exception e1) {
+                        throw new RuntimeException(e1);
+                    }
+                }
+                final String name = nameObject.toString();
+                if (multiModuleMode) {
+                    final ModuleElement msym = classElement.getEnclosingElement().getKind() == MODULE
+                        ? (ModuleElement)classElement.getEnclosingElement()
+                        : (ModuleElement)getPackageElement(classElement).getEnclosingElement();
+                    try {
+                        outLocn = fileManager.getLocationForModule(StandardLocation.CLASS_OUTPUT, msym.getQualifiedName().toString());
+                    } catch (IOException e1) {
+                        throw new UncheckedIOException(e1);
+                    }
+                } else {
+                    outLocn = StandardLocation.CLASS_OUTPUT;
+                }
+                final JavaFileObject outFile;
+                try {
+                    outFile = fileManager.getJavaFileForOutput(
+                        outLocn,
+                        name,
+                        JavaFileObject.Kind.CLASS,
+                        e.getSourceFile()
+                    );
+                } catch (IOException e1) {
+                    throw new UncheckedIOException(e1);
+                }
                 final byte[] input;
-                try (InputStream is = LastClassFileHolder.lastClassFile.openInputStream()) {
+                try (InputStream is = outFile.openInputStream()) {
                     final ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     final byte[] buffer = new byte[8192];
                     int n;
@@ -117,7 +125,7 @@ public class JavayieldJavacPlugin implements Plugin {
                 }
                 final byte[] output = YieldTransformer.transformClass(input);
                 if (output != null) {
-                    try (OutputStream os = LastClassFileHolder.lastClassFile.openOutputStream()) {
+                    try (OutputStream os = outFile.openOutputStream()) {
                         os.write(output);
                     } catch (Exception e1) {
                         trees.printMessage(
@@ -129,14 +137,20 @@ public class JavayieldJavacPlugin implements Plugin {
                 }
             }
         });
+    }
 
+    private static PackageElement getPackageElement(Element element) {
+        while (element.getKind() != ElementKind.PACKAGE) {
+            element = element.getEnclosingElement();
+        }
+        return (PackageElement)element;
+    }
+
+    private static <T extends Enum<T>> T getEnumMemberOrNull(Class<T> enumType, String name) {
         try {
-            inst.retransformClasses(Class.forName("com.sun.tools.javac.main.JavaCompiler"));
-            inst.appendToSystemClassLoaderSearch(new JarFile(
-                new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI())
-            ));
-        } catch (Exception e) {
-            throw new Error(e);
+            return Enum.valueOf(enumType, name);
+        } catch (IllegalArgumentException e) {
+            return null;
         }
     }
 }
--- javac/src/main/java/io/github/gaming32/javayield/javac/LastClassFileHolder.java
+++
@@ -1,7 +0,0 @@
-package io.github.gaming32.javayield.javac;
-
-import javax.tools.JavaFileObject;
-
-public class LastClassFileHolder {
-    public static JavaFileObject lastClassFile;
-}
